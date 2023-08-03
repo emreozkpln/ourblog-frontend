7e233bd860117ce6b4cd04f56a7bf38802bcc755
BestSuperUser <egeakay68@gmail.com>
2023-08-03 16:35:30 +0300

    Blog detayında markdown ve codeWrite beraber oldu

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -4,7 +4,19 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 
 function CodeDetail({ params }) {
-	const code = `	"use client";
+const markdownContent = `
+#### Hello, **Markdown** in Next.js with Tailwind CSS!
+
+This is a _demonstration_ of using Markdown and Tailwind CSS together in a Next.js project.
+
+1. Emre
+2. Ege
+3. Arda
+
+[Google](https://www.google.com.tr/)
+
+\`\`\`js
+"use client";
 	import React from "react";
 	import SyntaxHighlighter from "react-syntax-highlighter";
 	import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
@@ -19,25 +31,24 @@ function CodeDetail({ params }) {
 		);
 	}
 	
-	export default CodeWrite;`;
-
-	const markdownContent = `
-#### Hello, **Markdown** in Next.js with Tailwind CSS!
-
-This is a _demonstration_ of using Markdown and Tailwind CSS together in a Next.js project.
-
-1. Emre
-2. Ege
-3. Arda
-
-[Google](https://www.google.com.tr/)
-
+	export default CodeWrite;
+\`\`\`
 `;
 	return (
 		<div className="flex flex-col items-center">
 			<Header />
-			<ReactMarkdown className="text-blue-400" children={markdownContent} />
-			<CodeWrite children={code} />
+			<ReactMarkdown className="text-blue-400" children={markdownContent} components={{
+				code({ node, inline, className, children, ...props }) {
+					const match = /language-(\w+)/.exec(className || "");
+					return !inline && match ? (
+						<CodeWrite language={match[1]} children={String(children)} />
+					) : (
+						<code className={className} {...props}>
+							{children}
+						</code>
+					);
+				}
+			}}/>
 		</div>
 	);
 }
